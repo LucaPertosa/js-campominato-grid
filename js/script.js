3cb0c8e39a9035131429a58d07b0a409b4ccc08b
LucaPertosa <luca.pertosa@hotmail.it>
2023-03-20 13:30:01 +0100

    Project refactoring

--- js/script.js
+++ js/script.js
@@ -9,41 +9,30 @@ const difficultySelect = document.getElementById('difficulty-level');
 // Assegno al bottone l'event listener con la funzione creata per creare la griglia
 createGridButton.addEventListener('click', createGrid)
 
-
 function createGrid() {
+    grid.innerHTML = '';
     // Creo variabile per ottenere value selezionato dal menu a tendina
     const difficultyLevel = parseInt(difficultySelect.value);
     let numberOfCell = difficultyLevel;
 
+    // Modifica il valore della variabile CSS :root in base alla selezione del menu per gestire il numero di colonne
+    if (numberOfCell === 100) {
+        document.documentElement.style.setProperty('--number-of-cell', '10');
+    } else if (numberOfCell === 81) {
+        document.documentElement.style.setProperty('--number-of-cell', '9');
+    } else if (numberOfCell === 49) {
+        document.documentElement.style.setProperty('--number-of-cell', '7');
+    }
+    
     // Creo ciclo for per creare dinamicamente le box in base al numberOfCells
-    if (grid.childElementCount !== numberOfCell) {
-        grid.innerHTML = '';
-        for (let i = 1; i <= numberOfCell; i++) {
-            const currentNumber = i;
-            // inserisco il numero all'interno dello span utilizzando la funzione creata in precedenza
-            const newItem = generateGridItem(currentNumber);
-            // Assegno event listener per colorare il background al click
-            newItem.addEventListener("click", handleItemClick)
-            grid.append(newItem);
-            // Assegno al menu a tendina l'event listener per gestire i grid-item presenti in una riga
-            difficultySelect.addEventListener('change', function() {
-                // Seleziona la radice del documento
-                const root = document.documentElement;
-            
-                // Modifica il valore della variabile CSS in base alla selezione
-                if (this.value === '100') {
-                    root.style.setProperty('--number-of-cell', '10');
-                    grid.innerHTML = '';
-                } else if (this.value === '81') {
-                    root.style.setProperty('--number-of-cell', '9');
-                    grid.innerHTML = '';
-                } else if (this.value === '49') {
-                    root.style.setProperty('--number-of-cell', '7');
-                    grid.innerHTML = '';
-                }
-            });
-        };   
-    };
+    for (let i = 1; i <= numberOfCell; i++) {
+        const currentNumber = i;
+        // inserisco il numero all'interno dello span utilizzando la funzione creata in precedenza
+        const newItem = generateGridItem(currentNumber);
+        // Assegno event listener per colorare il background al click
+        newItem.addEventListener("click", handleItemClick)
+        grid.append(newItem);
+    };       
 };
   
 // FUNCIONS
@@ -58,7 +47,7 @@ function generateGridItem(text) {
     return newSquare;
 };
 
-// Funzione click play-button
+// Funzione click su box
 function handleItemClick() {
     this.classList.toggle("clicked");
 };
